00328040a0f201edf8196f6f2ce7b81cff37b3c7
softengineerstevens <softengineerstevens@gmail.com>
2024-07-09 22:10:52 +0300

    event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
-}
+function addingEventListener(){
+    const input = document.getElementById('button');
+    input.addEventListener('click', () => alert('Check ID Validity'));
+   };
+console.log(addingEventListener());
